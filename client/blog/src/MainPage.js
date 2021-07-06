90c43e7937d56e4fe25a8bd9d9328e07819bc3eb
zBiggui <bigguisenna@gmail.com>
2021-07-06 16:28:38 -0300

    Fixing unique key when map

--- client/blog/src/MainPage.js
+++ client/blog/src/MainPage.js
@@ -16,8 +16,11 @@ function MainPage() {
     }, [])
 
     const LikePost = (id) => {
+        alert("you liked a post")
         Axios.post(`http://localhost:3002/api/like/${id}`).then((response) => {
+            console.log(response)
             alert("you liked a post")
+            console.log("Laikei")
         })
     }
     return (
@@ -25,11 +28,11 @@ function MainPage() {
             <div className="PostContainer">
                 {postList.map((val, key) => {
                     return (
-                        <div className="Post" >
-                            <h1 className="post-title" onClick={() => (history.push(`/post/${key + 1}`))}>{val.title} {key + 1}</h1>
+                        <div className="Post" key={key}>
+                            <h1 className="post-title" onClick={() => (history.push(`/post/${key + 1}`))}>{val.title}</h1>
                             <p>{val.post_text.length > 300 ? val.post_text.substring(0, 300) + " ..." : val.post_text}</p>
                             <h4>{val.user_name}</h4>
-                            <button className="like_btn" onClick={(() => LikePost(val.id))}>Like</button>
+                            <button className="like_btn" onClick={(() => LikePost(key + 1))}>Like</button>
 
                             <h5>Likes: {val.likes}</h5>
                         </div>
